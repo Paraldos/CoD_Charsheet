ea9d751b971cb606d356d1b1e3a66b97c710965d
Lil Cthulhu <hillbrecht.s@gmail.com>
2022-08-13 23:20:58 +0200

    dice

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
 "use strict";
 console.log("started");
 
-console.log(CHARACTER);
+/* ====================
+Dice
+==================== */
+let numberOfDice = 0;
+let diceBox_value = document.querySelector(".diceBox_value");
+let diceBox_minus = document.querySelector(".diceBox_minus");
+let diceBox_plus = document.querySelector(".diceBox_plus");
+
+diceBox_minus.addEventListener("click", function () {
+  if (numberOfDice <= 0) return;
+  numberOfDice -= 1;
+  _change_dice_value(numberOfDice);
+});
+
+diceBox_plus.addEventListener("click", function () {
+  numberOfDice += 1;
+  _change_dice_value(numberOfDice);
+});
+
+let _change_dice_value = (value) => (diceBox_value.innerHTML = value);
